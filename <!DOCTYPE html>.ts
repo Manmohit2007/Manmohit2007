14fa25110ec204f3cef0815a53e65fdfd4fb26de
Manmohit2007 <manmohitsinghaps@gmail.com>
2025-11-06 23:52:03 +0530

    Change title from 'FanPlusFollow Viral Content Generator' to 'FanPlusFollow'

--- <!DOCTYPE html>.ts
+++ <!DOCTYPE html>.ts
@@ -4,7 +4,7 @@
     <link rel="icon" type="image/png" href="favicon.png" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FanPlusFollow Viral Content Generator</title>
+    <title>FanPlusFollow</title>
     <!-- Load Tailwind CSS from CDN --><script src="https://cdn.tailwindcss.com"></script>
     <style>
         /* Import Inter Font */
